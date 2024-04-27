fec6ae1ec67295816c3d287dc6b59adc9a5127c8
Ankush-Paul-02 <ankushpaulclg2002@gmail.com>
2024-04-27 14:34:27 +0530

    add category buttons

--- lib/feature/home/screens/home_screen.dart
+++ lib/feature/home/screens/home_screen.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 import 'package:velocity_x/velocity_x.dart';
 
+import '../widgets/custom_category_title_button.dart';
+import '../widgets/home_header.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -19,60 +22,38 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                height: 30.h,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.w),
-                  gradient: const LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [
-                      Colors.black,
-                      Colors.grey,
-                    ],
-                  ),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+              const HomeHeader(),
+              20.heightBox,
+              'Item categories'.text.bold.size(20).make(),
+              20.heightBox,
+              SizedBox(
+                height: 60,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
                   children: [
-                    Row(
-                      children: [
-                        const Spacer(),
-                        'FindersUnited'.text.white.size(20).make(),
-                        const Spacer(),
-                        const CircleAvatar(
-                          backgroundImage: NetworkImage(
-                            'https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D',
-                          ),
-                        ),
-                      ],
+                    CustomCategoryTitleButton(
+                      category: 'All',
+                      onTap: () {},
                     ),
-                    const Spacer(),
-                    30.heightBox,
-                    'Discover Lost Items'.text.size(22).bold.white.make(),
-                    20.heightBox,
-                    SearchBar(
-                      backgroundColor: MaterialStateProperty.all(Colors.white),
-                      leading: const Icon(
-                        Icons.search,
-                        color: Colors.grey,
-                      ),
-                      hintText: 'Search item',
-                      hintStyle: MaterialStateProperty.all(
-                        const TextStyle(
-                          color: Colors.grey,
-                        ),
-                      ),
-                      trailing: [
-                        Icon(
-                          Icons.filter_alt_rounded,
-                          color: Colors.cyan[200],
-                        ),
-                      ],
+                    CustomCategoryTitleButton(
+                      category: 'Wallet',
+                      onTap: () {},
+                    ),
+                    CustomCategoryTitleButton(
+                      category: 'Watch',
+                      onTap: () {},
+                    ),
+                    CustomCategoryTitleButton(
+                      category: 'Aadhaar',
+                      onTap: () {},
+                    ),
+                    CustomCategoryTitleButton(
+                      category: 'Pan Card',
+                      onTap: () {},
                     ),
                   ],
-                ).pSymmetric(h: 5.w, v: 5.w),
+                ),
               )
             ],
           ).pSymmetric(h: 5.w, v: 5.w),
---
+++ lib/feature/home/widgets/custom_category_title_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class CustomCategoryTitleButton extends StatelessWidget {
+  final String category;
+  final VoidCallback onTap;
+
+  const CustomCategoryTitleButton({
+    super.key,
+    required this.category,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        width: 30.w,
+        height: 45,
+        margin: EdgeInsets.only(right: 5.w),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          gradient: LinearGradient(
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            colors: [
+              Colors.cyan.shade100,
+              Colors.cyan,
+            ],
+          ),
+        ),
+        child: category.text.semiBold.size(18).white.makeCentered(),
+      ),
+    );
+  }
+}
